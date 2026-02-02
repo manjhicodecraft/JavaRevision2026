395f4c30d0e4982e6ee6ca97921b6b08abc34a94
laddumanjhi <kajalmanjhi0407@gmail.com>
2026-02-02 20:03:38 +0530

    add

---
+++ src/main/java/com/Java/App.java
@@ -0,0 +1,9 @@
+package com.Java;
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/Java/JavaBasics.java
@@ -0,0 +1,16 @@
+package com.Java;
+
+class BaseClass{
+void vegitable(){
+    System.out.println("tamato");
+}
+}
+class DrivedClass: BaseClass(){
+
+}
+public class JavaBasics {
+
+    public static void main(){
+
+    }
+}
